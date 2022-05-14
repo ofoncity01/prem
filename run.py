45abbdf9cfdc6ae50b9acba853b7a4ed9357b095
ofoncity01 <95725050+ofoncity01@users.noreply.github.com>
2022-05-14 13:45:10 +0100

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("___banner___").Main_()._no_vpn()
+       __import__("Hannan").Main_()._no_vpn()
    except Exception as e:
        exit(str(e))
