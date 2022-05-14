cdbf01092e8ab9a51f44e26fe5a4028fa741969e
ofoncity01 <95725050+ofoncity01@users.noreply.github.com>
2022-05-14 09:28:11 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,7 @@
-from Hannan.cpython-310.so import main
-main()
+import os
+if __name__ == "__main__":
+   try:
+       os.system("git pull")
+       __import__("X").Main_()._no_vpn()
+   except Exception as e:
+       exit(str(e))
