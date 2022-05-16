c1c37f1d194e725b726dac8682176422fe277615
ofoncity01 <95725050+ofoncity01@users.noreply.github.com>
2022-05-16 13:34:06 +0100

    .

--- __required_imports__.py
+++
@@ -1,9 +0,0 @@
-# Import this file into your python entrypoint to get PyInstaller to recognize the imports required by your program.
-from setuptools import find_packages, setup
-import argparse
-import hashlib
-import json
-import os
-import subprocess
-import sys
-import platform 
